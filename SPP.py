6fe6073dfc18aa0b776be43faed60fa56a4e0963
Onyekachukwu Okonji <40724187+onyekaokonji@users.noreply.github.com>
2022-09-25 23:41:08 +0100

    Update SPP.py

--- SPP.py
+++ SPP.py
@@ -8,10 +8,10 @@ def SPP(self, previous_conv_layer, previous_conv_layer_size, output_pool_size=[4
     for i in range(len(output_pool_size)):
         window_height = int(math.ceil(previous_conv_layer_size[0]/output_pool_size[i]))
         window_width = int(math.ceil(previous_conv_layer_size[1]/output_pool_size[i]))
-        window_stride_x = int(math.floor(previous_conv_layer_size[0]/output_pool_size[i]))
-        window_stride_y = int(math.floor(previous_conv_layer_size[1]/output_pool_size[i]))
-        window_height_pad = int((window_height*output_pool_size[i] - previous_conv_layer_size[0] + 1)/2)
-        window_width_pad = int((window_width*output_pool_size[i] - previous_conv_layer_size[1] + 1)/2)
+        window_stride_x = int(math.ceil(previous_conv_layer_size[0]/output_pool_size[i]))
+        window_stride_y = int(math.ceil(previous_conv_layer_size[1]/output_pool_size[i]))
+        window_height_pad = math.floor((window_height*output_pool_size[i] - previous_conv_layer_size[0] + 1)/2)
+        window_width_pad = math.floor((window_width*output_pool_size[i] - previous_conv_layer_size[1] + 1)/2)
 
         maxpool = nn.MaxPool2d(kernel_size=(window_height, window_width), stride=(window_stride_x, window_stride_y), padding = (window_height_pad, window_width_pad))
         x = maxpool(previous_conv_layer)
